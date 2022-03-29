c2ca7ed4854eaaae8a92d2bda3146e3be70e6c17
Marcin Fiorek <marcin.fiorek@admiralgroup.co.uk>
2022-03-29 15:48:08 +0200

    Remove unused files and replace boilerplate code

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-800">
+      <h1 className="text-white text-6xl font-bold">Skilltackerthon</h1>
+    </div>
+  );
+}
+
+export default App;
